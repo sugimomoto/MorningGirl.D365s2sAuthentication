47614fbf786ec36f5a4c7f279ec7bbc774104dbb
sugimomoto@gmail.com <sugimomoto@gmail.com>
2017-03-07 21:59:43 +0900

    プロジェクト ファイルを追加します。

---
+++ MorningGirl.D365s2sAuthentication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorningGirl.D365s2sAuthentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
